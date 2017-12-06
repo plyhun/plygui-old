b629d0ca3578cade8e8240da9d14353f4570cade
Serhii Plyhun <snuk188@gmail.com>
2017-12-06 16:42:46 +0100

    API externalize

--- src/development/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod layout;
-
-pub const CLASS_ID_APPLICATION: &str = "PlyguiApplication";
-pub const CLASS_ID_WINDOW: &str = "PlyguiWindow";
-pub const CLASS_ID_BUTTON: &str = "PlyguiButton";
-pub const CLASS_ID_LAYOUT_LINEAR: &str = "PlyguiLinearLayout";
-
