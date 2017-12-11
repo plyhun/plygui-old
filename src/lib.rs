878c8c6ef5baf03b86846045f42c3777b78fde58
Serhii Plyhun <snuk188@gmail.com>
2017-12-11 09:54:15 +0100

    Cocoa deps removed

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,3 @@
-/*#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate derive_builder;
-
-#[cfg(target_os="windows")]
-mod win32;
-#[cfg(target_os="windows")]
-use win32 as inner;
-#[cfg(target_os="windows")]
-pub use inner::common;
-
-#[cfg(target_os = "macos")]
-#[macro_use]
-extern crate objc;
-
-#[cfg(target_os = "macos")]
-mod cocoa;
-#[cfg(target_os = "macos")]
-use cocoa as inner;
-#[cfg(target_os = "macos")]
-pub use inner::common;*/
-
 extern crate plygui_api;
 
 pub use plygui_api::traits::*;
