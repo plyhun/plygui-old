3d58f104f6350d2ee3f5fe534a33a1d2d17b3019
Serhii Plyhun <snuk188@gmail.com>
2017-12-12 10:56:11 +0100

    Features management

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,13 @@ pub use plygui_api::types::*;
 pub use plygui_api::layout;
 pub use plygui_api::members;
 pub use plygui_api::utils;
+
+#[cfg(all(target_os = "macos", feature = "cocoa"))]
+extern crate plygui_cocoa;
+#[cfg(all(target_os = "macos", feature = "cocoa"))]
+pub use plygui_cocoa::*;
+
+#[cfg(all(target_os = "windows", feature = "win32"))]
+extern crate plygui_win32;
+#[cfg(all(target_os = "windows", feature = "win32"))]
+pub use plygui_win32::*;
