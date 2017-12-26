922f0bcaada18ff6216db6b9294cd19c20815e66
snuk182 <snuk188@gmail.com>
2017-12-26 20:18:40 +0100

    Callbacks work

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate plygui_api;
 pub use plygui_api::traits::*;
 pub use plygui_api::ids::*;
 pub use plygui_api::types::*;
+pub use plygui_api::callbacks;
 pub use plygui_api::layout;
 pub use plygui_api::members;
 pub use plygui_api::utils;
