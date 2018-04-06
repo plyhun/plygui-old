d9893ec5107936c9c0704ef7c135635ffee5a7fd
snuk182 <snuk188@gmail.com>
2018-04-06 18:58:22 +0200

    oop3 :(

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,6 @@ pub use plygui_api::types::*;
 pub use plygui_api::callbacks;
 pub use plygui_api::layout;
 pub use plygui_api::members;
-pub use plygui_api::utils;
-
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
-extern crate plygui_gtk;
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
-pub use plygui_gtk::*;
-
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
-extern crate plygui_qt;
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
-pub use plygui_qt::*;
-
-#[cfg(all(target_os = "macos", feature = "cocoa"))]
-extern crate plygui_cocoa;
-#[cfg(all(target_os = "macos", feature = "cocoa"))]
-pub use plygui_cocoa::*;
 
 #[cfg(all(target_os = "windows", feature = "win32"))]
 extern crate plygui_win32;
