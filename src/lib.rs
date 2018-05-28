e1ce7551310db85ac9643f955fb24755932d8848
Serhii Plyhun <snuk188@gmail.com>
2018-05-28 11:15:45 +0200

    Get rid of prefixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
 extern crate plygui_api;
 
-pub use plygui_api::traits::*;
+pub use plygui_api::controls::*;
 pub use plygui_api::ids::*;
 pub use plygui_api::types::*;
 pub use plygui_api::callbacks;
 pub use plygui_api::layout;
+pub use plygui_api::utils;
+
+#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
+pub extern crate plygui_gtk as imp;
+
+#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
+pub extern crate plygui_qt as imp;
+
+#[cfg(all(target_os = "macos", feature = "cocoa"))]
+pub extern crate plygui_cocoa as imp;
 
 #[cfg(all(target_os = "windows", feature = "win32"))]
-extern crate plygui_win32;
-#[cfg(all(target_os = "windows", feature = "win32"))]
-pub use plygui_win32::*;
+pub extern crate plygui_win32 as imp;
