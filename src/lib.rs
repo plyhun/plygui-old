339d77f5f8a9a848deb12ad3e08066f59de2bd14
snuk182 <snuk188@gmail.com>
2018-09-08 21:43:07 +0200

    Remove Qt for now

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,6 @@ pub use plygui_api::utils;
 #[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
 pub extern crate plygui_gtk as imp;
 
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
-pub extern crate plygui_qt as imp;
-
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
 pub extern crate plygui_cocoa as imp;
 
