3cb98bd3a3069966b9043fcac0010054f2573ef5
Serhii Plyhun <snuk188@gmail.com>
2018-09-17 16:17:02 +0200

    Get all back

--- src/api/src/members.rs
+++
@@ -1,7 +0,0 @@
-pub type MemberId = &'static str;
-
-pub const MEMBER_ID_APPLICATION: MemberId = "PlyguiApplication";
-pub const MEMBER_ID_WINDOW: MemberId = "PlyguiWindow";
-pub const MEMBER_ID_BUTTON: MemberId = "PlyguiButton";
-pub const MEMBER_ID_LAYOUT_LINEAR: MemberId = "PlyguiLinearLayout";
-pub const MEMBER_ID_FRAME: MemberId = "PlyguiFrame";
--- src/backend/win32/build.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(feature = "manifest")]
-extern crate embed_resource;
-
-fn main() {
-    #[cfg(feature = "manifest")]
-	embed_resource::compile("plygui.rc");
-}
