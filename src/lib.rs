028e7148401b4a86d808f45f80c6274545f7e870
Serhii Plyhun <snuk188@gmail.com>
2019-03-28 13:19:28 +0100

    Small refactor

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,10 @@ pub use plygui_api::layout;
 pub use plygui_api::utils;
 
 #[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
-extern crate plygui_gtk;
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
 pub use plygui_gtk::prelude::*;
 
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
-extern crate plygui_cocoa;
-#[cfg(all(target_os = "macos", feature = "cocoa"))]
 pub use plygui_cocoa::prelude::*;
 
-#[cfg(all(target_os = "windows", feature = "win32"))]
-extern crate plygui_win32;
 #[cfg(all(target_os = "windows", feature = "win32"))]
 pub use plygui_win32::prelude::*;
